0aeca2e35424eca440f6355a7ee91b13f8e7a8aa
Luke Hudson <lucas@speak.geek.nz>
2025-11-30 23:48:16 +0100

    fix: describe stove gui

--- src/App.svelte
+++ src/App.svelte
@@ -7,11 +7,13 @@
 <main>
   <h1>Date cooker</h1>
 
+
   <div class="card">
     <DateCooker bind:value={theDate}/>
 
     <hr>
 
+    <p>My cooker uses this interface to control the stove elements, now you can enjoy using it to pick a date!</p>
     <div style="font-size: 1.5rem">Date set to: <br>{theDate.toLocaleString()}</div>
   </div>
 </main>
